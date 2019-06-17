3ac9d4fcfd5deaac3a965fe76cb3b6e113ac1916
Ross Taylor <rj-taylor@live.co.uk>
2019-06-17 19:26:42 +0100

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,9 @@
-dependencies = ["torch", "math"]
+dependencies = ["torch", "torchvision", "scipy"]
 
+import torchvision.transforms as transforms
+from sotabench.image_classification import imagenet, cifar10
+import torch
+import PIL
 
 def se_resnet20(**kwargs):
     from senet.se_resnet import se_resnet20 as _se_resnet20
@@ -23,3 +27,23 @@ def se_resnet101(**kwargs):
     from senet.se_resnet import se_resnet101 as _se_resnet101
 
     return _se_resnet101(**kwargs)
+
+def benchmark():
+    hub_model = torch.hub.load('moskomule/senet.pytorch', 'se_resnet50', pretrained=True,)
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    input_transform = transforms.Compose([
+        transforms.Resize(256, PIL.Image.BICUBIC),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    
+    cifar10.benchmark(
+        model=hub_model,
+        paper_model_name='SE-ResNet-50',
+        paper_arxiv_id='1709.01507',
+        paper_pwc_id='squeeze-and-excitation-networks',
+        input_transform=input_transform,
+        batch_size=256,
+        num_gpu=1
+    )
